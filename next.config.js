1a9df01fb9389d729ec206ce9a322dd7c6259cd7
skarthik05 <92262232+skarthik05@users.noreply.github.com>
2023-01-12 10:45:16 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,5 +3,13 @@ module.exports = {
       // Warning: This allows production builds to successfully complete even if
       // your project has ESLint errors.
       ignoreDuringBuilds: true,
+        exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+    }
+  },
     },
   }
